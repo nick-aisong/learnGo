63eac0213f9a00a45da4d4cba8961f4df42742fb
nick-aisong <nick_aisong@163.com>
2019-10-22 17:03:37 +0530

    add files

--- 学习Go语言/ch07_communication_exercise/Q29_单词和字母统计.go
+++ 学习Go语言/ch07_communication_exercise/Q29_单词和字母统计.go
@@ -4,14 +4,29 @@ Q29. (0) 单词和字母统计
 	1. 计算字符数量（包括空格）；
 	2. 计算单词数量；
 	3. 计算行数。
-换句话说，实现一个 wc(1)，然而只需要从标准输入读取。
+换句话说，实现一个 wc，然而只需要从标准输入读取。
 */
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	var chars, words, lines int
+	r := bufio.NewReader(os.Stdin)
+	for {
+		switch s, ok := r.ReadString('\n'); true {
+		case ok != nil:
+			fmt.Printf("%d %d %d\n", chars, words, lines)
+			return
+		default:
+			chars += len(s)
+			words += len(strings.Fields(s))
+			lines++
+		}
+	}
 }
