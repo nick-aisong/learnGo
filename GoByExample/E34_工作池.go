b7767e147601bab8816d3d83aac677d918c3fde8
nick-aisong <nick_aisong@163.com>
2019-11-03 01:03:04 +0800

    add files

---
+++ GoByExample/E34_工作池.go
@@ -0,0 +1,35 @@
+// E34_工作池
+// 在这个例子中，我们将看到如何使用 Go 协程和通道实现一个工作池
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Println("worker", id, "processing job", j)
+		time.Sleep(time.Second)
+		results <- j * 2
+	}
+}
+
+func main() {
+
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	for j := 1; j <= 9; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	for a := 1; a <= 9; a++ {
+		<-results
+	}
+}
