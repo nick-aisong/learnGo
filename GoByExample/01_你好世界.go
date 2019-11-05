e4dbbb3760586425fd2ad32e650cc2d97e368110
nick_aisong <nick_aisong@163.com>
2019-11-06 01:32:39 +0800

    add files

---
+++ GoByExample/01_你好世界.go
@@ -0,0 +1,10 @@
+// 01_你好世界
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
---
+++ GoByExample/45_字符串格式化.go
@@ -0,0 +1,10 @@
+// 45_字符串格式化
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
